96f093bbf9907f9a5887fec354efeece70631e4f
Levente Otta <leventeotta@gmail.com>
2020-02-29 15:17:45 +0100

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'sshy'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
--- webpack.mix.js
+++
@@ -1,5 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('src/renderer/app.js', 'dist/');
-mix.sass('src/renderer/sass/app.scss', 'dist/');
-mix.setPublicPath('dist');
